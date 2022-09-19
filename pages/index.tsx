308d30ca2bd49058a7b9ead2ab430d2ffab13517
Katie <katie@katies-imac.lan>
2022-09-19 09:12:28 -0700

    importing fonts in globals.scss

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,9 @@ const Home: NextPage = () => {
         <title>Poke Bowl</title>
         <meta name="description" content="Fresh and Healthy Poke Bowl" />
         <link rel="icon" href="/pokebowl.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Quattrocento&family=Quattrocento+Sans:wght@400;700&display=swap" rel="stylesheet"></link>
       </Head>
       <main>
         <motion.div
